ea852bd9655626888c8cf0980171830c8c8a8356
刘瑶 <307956355@qq.com>
2017-05-04 09:56:25 +0800

    fix

---
+++ public/js/admin/cash/cash-audit.js
@@ -0,0 +1,9 @@
+/**
+ * Created by liuyao on 2017/5/4.
+ */
+define(["angular"],function (angular) {
+    var cashAudit = angular.module("cashAudit", ['ui.bootstrap', 'ngAnimate', 'ui.router', "model"]);
+
+    
+
+});
